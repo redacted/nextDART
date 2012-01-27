8af03d0d54217a17675ab88e7bd069b711f2a184
Steven Tobin <tobin2402 (at) gmail.com>
2012-01-27 14:40:55 +0000

    irishrail.ie changes require screenscraping rather than RSS parsing

--- nextDART.py
+++ nextDART.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+import sys
+from optparse import OptionParser
+import urllib2
+
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    print "Requires BeautifulSoup module"
+    sys.exit(0)
+    
 # Global data
 
 # dict for dart station rss IDs
@@ -11,31 +21,11 @@ stations = {
     'tara street'      : 'TARA',
 }   
 
-
-def dart_arrival_time(dart, now):
-    """
-    returns time to DART in minutes (int). 
-    dart = RSS item 
-    now = (hours, minutes) tuple
-    """
-
-    dart_ = dart.split()
-    # get the hours and minutes of the dart arrival, convert to minutes
-    hours_eta, mins_eta = dart_[0].split(':')
-    delta_min = 60*int(hours_eta) + int(mins_eta)
-
-    # do the same with the current time
-    now_min = 60*int(now[0]) + int(now[1])
-
-    return int(delta_min - now_min)
-
 def set_args():
     """
     Set up options
     """
 
-    import sys
-    from optparse import OptionParser
 
     # set up arguments, take any immediate action required
     parser = OptionParser()
@@ -64,29 +54,14 @@ def set_args():
     return (start_station, direction)
 
 def main():
-    import sys
 
-    try:
-        import feedparser
-    except Exception, e:
-        print("Requires feedparser module")
-        print e
-
-    # handle options
     start_station, direction = set_args()
 
-    feed = feedparser.parse('http://www.irishrail.ie/realtime/publicinfo.asp?strLocation=' + stations[start_station])
-    # check that feed returned correctly
-    good_responses = [200,]
     try:
-        if feed.status not in good_responses:
-            print("Feed download failed (status %d)" % (feed.status) )
-            sys.exit()
-    except Exception, e:
-        print("Error: no connection made")
-        sys.exit()
-
-    print feed.feed.title
+        raw_response = urllib2.urlopen("http://www.irishrail.ie/realtime/station_details.jsp?ref=" + stations[start_station], )
+    except urllib2.HTTPError:
+        print "Unable to connect"
+        sys.exit(0)  
 
     # terminating stations
     if direction == 'south':
@@ -94,20 +69,24 @@ def main():
     elif direction == 'north':
         targets = ['Howth', 'Malahide']
 
-    # initialise current time
-    feed_time = feed.feed.title.split()[-1]
-    # time is HH:MMam so we split on':' then remove the am or pm
-    now_hour, now_min = feed_time.split(':')[0], feed_time.split(':')[1]
-    now_min = now_min[0:-2]
-    now = (now_hour, now_min)
+    tables = BeautifulSoup(raw_response.read()).findAll('table')
 
-    # now find the right darts
-    #for item in feed['items']:
-    for entry in feed.entries:
-        dart = entry.title
-        # destination is last word in each item
-        if dart.split()[-1] in targets:
-            print("[ arriving in %3d minutes ]  %s" % (dart_arrival_time(dart, now), dart))     
+    trains = []
+    for tb in tables:
+        rows = tb.findAll('tr')
+        for tr in rows:
+            train = []
+            cols = tr.findAll('td')
+            for td in cols:
+                text_ = td.find(text=True)
+                if text_ is not None:
+                    text = ''.join(text_)
+                    train.append(text)
+            if "DART" in train: trains.append(train)                   
+
+    for train in trains:
+        if train[2] in targets:
+            print("[ arriving in %s]  %s" % (train[6], train[3]+" "+train[0])) 
 
 if __name__ == '__main__':
     main()
